5f398d47e71b832d69887c5d5637403e69fbd710
Micheal Limones <Micheal.Limones@du.edu>
2021-08-03 20:46:56 -0600

    added api keys and general function.

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,20 +5,26 @@
 // >> results to display:
 // >> >> national park name with image, 3 activies, weather
 // link/button to take you back to homepage {{nav bar}}
-// added line for merge stuff -- adding to revert change 
+// added line for merge stuff
 
 // create varible that matches with button >> verify botton tags
 var searchBtn = document.querySelector('.btn');
 // create varible that matches with state >> verify botton tags
-var selectedState = document.querySelector('.state')
+var selectedState = document.querySelector('.state');
 
 //API Keys:
 var npsApiKey = keUgXA4zA0DCR17ihQfTmtASQqGBGyMJ8Q85tkNc;
 var weatherApiKey = f6dbccad1096ef580392335246d5632e;
 
 var renderSearchResults = function (event) {
-
-    var npsApiUrl = 
+  //will need to come back to this to determine if we need state code or if we can use fill state name
+  //limit is currently set to 50. we will want to discuess how many results we want it to return on search
+  //adding another line for testing
+  var npsApiUrl =
+    'https://developer.nps.gov/api/v1/parks?stateCode=' +
+    stateCode +
+    '&limit=50&api_key=' +
+    npsApiKey;
 };
 
 searchBtn.addEventListener('click', renderSearchResults);
