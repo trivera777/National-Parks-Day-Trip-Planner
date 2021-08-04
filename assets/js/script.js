609cb62eea72731a04a94024cb7539372d40a713
Micheal Limones <Micheal.Limones@du.edu>
2021-08-03 20:40:00 -0600

    added api keys to js file. created renderSearchResults function.

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,4 +5,20 @@
 // >> results to display:
 // >> >> national park name with image, 3 activies, weather
 // link/button to take you back to homepage {{nav bar}}
-// added line for merge stuff
+// added line for merge stuff -- adding to revert change 
+
+// create varible that matches with button >> verify botton tags
+var searchBtn = document.querySelector('.btn');
+// create varible that matches with state >> verify botton tags
+var selectedState = document.querySelector('.state')
+
+//API Keys:
+var npsApiKey = keUgXA4zA0DCR17ihQfTmtASQqGBGyMJ8Q85tkNc;
+var weatherApiKey = f6dbccad1096ef580392335246d5632e;
+
+var renderSearchResults = function (event) {
+
+    var npsApiUrl = 
+};
+
+searchBtn.addEventListener('click', renderSearchResults);
