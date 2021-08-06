9471bae4e8d0230f0398ff3b00e68f19a33dc0f1
Nathan Flessner <nathanflessner1@gmail.com>
2021-08-05 20:13:53 -0600

    added second javascript file and got state code on activites page

---
+++ assets/js/activities.js
@@ -0,0 +1,42 @@
+// user selectes state from drop down
+// user clicks submit
+// on button click it will take us to the results page
+// page will render nation park results
+// >> results to display:
+// >> >> national park name with image, 3 activies, weather
+// link/button to take you back to homepage {{nav bar}}
+// added line for merge stuff
+//API Keys:
+var npsApiKey = 'keUgXA4zA0DCR17ihQfTmtASQqGBGyMJ8Q85tkNc';
+var weatherApiKey = 'f6dbccad1096ef580392335246d5632e';
+let stateCode = JSON.parse(sessionStorage.getItem('stateCode'));
+
+
+var renderSearchResults = function (stateCode) {
+  var npsApiUrl =
+    'https://developer.nps.gov/api/v1/parks?stateCode=' +
+    stateCode +
+    '&limit=1&api_key=' +
+    npsApiKey;
+
+  console.log(npsApiUrl);
+
+  
+  function getApiNps(npsApiUrl) {
+    sessionStorage.getItem('stateCode');
+    console.log(stateCode);
+    console.log('before fetch');
+    fetch(npsApiUrl).then(function (response) {
+      console.log(response);
+
+      if (response.status === 200) {
+        console.log('this worked');
+      }
+      return response.json();
+    });
+  }
+
+  getApiNps(npsApiUrl);
+
+};
+console.log(stateCode);
