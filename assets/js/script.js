1177bba1238789acea146613af3f8042b5d2f88c
Tifanny Rivera <trivera51580@gmail.com>
2021-08-05 18:58:29 -0600

    activities html columns done

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,16 +7,19 @@
 // link/button to take you back to homepage {{nav bar}}
 // added line for merge stuff
 
-// create varible that matches with button >> verify botton tags
-var searchBtn = document.querySelector('.btn');
-// create varible that matches with state >> verify botton tags
-var selectedState = document.querySelector('.state');
+// create varible that matches with button >> verify button tag
+// Nathan updated this.
+var searchBtn = $('.search-button');
+// this variable is not needed any more.
+// var selectedState = document.querySelector('.state');
+
+let redirectUrl = './activities.html';
+let stateCode = '';
 
 //API Keys:
-var npsApiKey = keUgXA4zA0DCR17ihQfTmtASQqGBGyMJ8Q85tkNc;
-var weatherApiKey = f6dbccad1096ef580392335246d5632e;
-
-var renderSearchResults = function (event) {
+var npsApiKey = 'keUgXA4zA0DCR17ihQfTmtASQqGBGyMJ8Q85tkNc';
+var weatherApiKey = 'f6dbccad1096ef580392335246d5632e';
+var renderSearchResults = function (stateCode) {
   //will need to come back to this to determine if we need state code or if we can use fill state name
   //limit is currently set to 50. we will want to discuess how many results we want it to return on search
   //adding another line for testing
@@ -25,6 +28,32 @@ var renderSearchResults = function (event) {
     stateCode +
     '&limit=50&api_key=' +
     npsApiKey;
+
+  console.log(npsApiUrl);
+
+  // this entire function needs to be reviewed
+  function getApi(npsApiUrl) {
+    console.log('before fetch');
+    //my fetch is not working.
+    //I commented out the redirect in the click function to see this info
+    fetch(npsApiUrl).then(function (response) {
+      console.log(response);
+      console.log('inside the fetch');
+      if (response.status === 200) {
+        console.log('here');
+      }
+      return response.json();
+    });
+  }
+  getApi();
 };
 
-searchBtn.addEventListener('click', renderSearchResults);
+searchBtn.click(function (event) {
+  event.preventDefault();
+  stateCode = $('.state-selection').find('option:selected').data('state');
+  console.log(stateCode);
+  sessionStorage.setItem('stateCode', stateCode);
+  // window.location.href = redirectUrl;
+  //call to the render search function
+  renderSearchResults(stateCode);
+});
